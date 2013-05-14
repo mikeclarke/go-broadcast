3f980b07dea62237b71eff9f8ad8062e189bc1b3
Mike Clarke <mike@disqus.com>
2013-05-14 15:02:59 -0700

    Add tests for Broadcaster

--- broadcast_test.go
+++ broadcast_test.go
@@ -1,9 +1,36 @@
 package broadcast
 
 import (
+	"log"
 	"testing"
+	"time"
 )
 
 func TestBroadcaster(t *testing.T) {
-	NewBroadcaster()
+	// Create testing harness
+	done := make(chan bool)
+
+	// Initialize broadcaster
+	b := NewBroadcaster()
+
+	// Start listener
+	outputc := b.Listen()
+
+	// Func to listen for output
+	go func() {
+		msg := <-outputc
+		log.Print(msg)
+		done <- true
+	}()
+
+	// Send that shit
+	b.Write(1)
+
+	// Wait for results to show up
+	select {
+	case <- done:
+		return
+	case <- time.After(2 * time.Second):
+		t.Fatalf("Messages timed out.")
+	}
 }
