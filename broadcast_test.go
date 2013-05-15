c23f028861d40ca6c0a9b8b4b38426c215177cfc
Mike Clarke <mike@disqus.com>
2013-05-14 17:27:32 -0700

    Better test coverage

--- broadcast_test.go
+++ broadcast_test.go
@@ -7,30 +7,51 @@ import (
 
 func TestBroadcaster(t *testing.T) {
 	// Create testing harness
-	done := make(chan Message)
+	got := []Message{}
+	listeners := []MessageChannel{}
+	output := make(chan Message)
+	done := make(chan bool)
 
 	// Initialize broadcaster
 	b := NewBroadcaster()
 
 	// Start listener
-	outputc := b.Listen()
 
-	// Func to listen for output
+	// Create 10 listeners, send results to shared `output` channel
+	for x := 0; x < 10; x++ {
+		new_listener := b.Listen()
+		listeners = append(listeners, new_listener)
+		t.Logf("Added listener %d", x)
+
+		go func(channel MessageChannel) {
+			msg := <-channel
+			t.Logf("Received data %s", msg)
+			output <- msg
+		}(new_listener)
+	}
+
+	// Collect all of the results from listeners
 	go func() {
-		msg := <-outputc
-		done <- msg
+		for msg := range output {
+			t.Logf("Received output %s", msg)
+			got = append(got, msg) 
+			if len(got) == 10 {
+				// Since all 10 messages were received, done indicates
+				// the test has passed.
+				done <- true
+			}
+		}
 	}()
 
-	// Send that shit
+	// Send that sample message
+	t.Logf("Writing data: 1")
 	b.Write(1)
 
 	// Wait for results to show up
 	select {
-	case msg := <- done:
-		if msg != 1 {
-			t.Fatalf("msg does not match")
-		}
-	case <- time.After(2 * time.Second):
-		t.Fatalf("Messages timed out.")
+	case <- done:
+		break
+	case <- time.After(1 * time.Second):
+		t.Fatalf("Messages were not received by listeners.")
 	}
 }
